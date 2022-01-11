a557e86477b9def26aecfcae234d18f68a0097bd
KyleOrr676 <Kyle.Orr@hotmail.com>
2022-01-11 12:40:05 +0000

    ascii practice

---
+++ src/practice/Main.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Variables to detect a certain character and assign it an ascii value
+
+        char ch = 'x';
+        int ascii = ch;
+
+        // Gets the ascii value of the inputted character and prints it as a string message
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+    }
+}
